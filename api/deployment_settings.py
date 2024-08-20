b423c5ebb779f385db1417816eb9270fc0d3edf8
CBI Analytics <cbi@email.com>
2024-08-20 17:09:56 +0200

    cors headers

--- api/deployment_settings.py
+++ api/deployment_settings.py
@@ -21,9 +21,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://render-deploy-tutorial-reactjs-code.onrender.com'
+]
 
 STORAGES = {
     "default":{
