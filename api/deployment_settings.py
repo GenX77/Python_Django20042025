408cf661f0072453fdd3b9bbd0ab059868381dbc
CBI Analytics <cbi@email.com>
2024-08-30 09:54:00 +0200

    500 error

--- api/deployment_settings.py
+++ api/deployment_settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .settings import * 
 from .settings import BASE_DIR
 
-#ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = True
@@ -29,9 +29,9 @@ STORAGES = {
     "default":{
         "BACKEND" : "django.core.files.storage.FileSystemStorage",
     },
-    "staticfiles": {
-        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
-    },
+    # "staticfiles": {
+    #     "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    # },
 
 }
 
