bd9eeba2d55d2c98184ecfe168ed767687c91f7b
GenX77 <gendersonc@gmail.com>
2025-04-16 11:55:34 +0200

    Ensure build.sh uses LF line endings

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
 set -o errexit
 
@@ -7,23 +6,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+
 # Crear superusuario automáticamente (si se define la variable de entorno)
 # if [[ $CREATE_SUPERUSER ]]
 # then
-#     python manage.py createsuperuser --no-input
+#   python manage.py createsuperuser --no-input
 # fi
-=======
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
-python manage.py migrate
-
-
-# if [[ $CREATE_SUPERUSER ]]
-# then
-#     python manage.py createsuperuser --no-input
-# fi
->>>>>>> 72ce678392f09cfcd2104b67e6b794cbbfb143bb
