98129b95131a963d16157390ab7f4aa5b90dab22
Gen_X <104616772+GenX77@users.noreply.github.com>
2025-04-16 12:28:00 +0200

    Update wspi.py

--- wspi.py
+++ wspi.py
@@ -1,12 +1,5 @@
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-# Obtener la ruta del directorio actual (donde está wsgi.py)
-path = os.path.dirname(os.path.abspath(__file__))
-# Añadir el directorio padre al path de Python
-if path not in sys.path:
-    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycalendar.settings')
 
