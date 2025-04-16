37036b37b498890b8efa1cf74d469323d0e39e53
Gen_X <104616772+GenX77@users.noreply.github.com>
2025-04-16 12:40:25 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,12 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mycalendar.settings')
+    # Obtener la ruta del directorio actual (donde está manage.py)
+    path = os.path.dirname(os.path.abspath(__file__))
+    # Añadir el directorio padre al path de Python
+    sys.path.insert(0, path)
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
