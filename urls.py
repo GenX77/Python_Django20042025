e6def1fcc55613fcdd9f03b604aa6fe0ca9b929b
Gen_X <104616772+GenX77@users.noreply.github.com>
2025-04-17 16:18:49 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.hola_mundo, name='hola'),
-    path('hola.html', views.hola_mundo, name='hola_html'),
+    path('prueba.php', views.hola_mundo, name='prueba_php'),
 ]
